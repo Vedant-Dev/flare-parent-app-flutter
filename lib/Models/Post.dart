b450f355075ccc25beb55fc63cee0fc774c5dfdc
Vedant-Dev <iamsinghved@gmail.com>
2020-07-02 20:53:47 +0530

    Add ActivityPage

--- lib/Models/Post.dart
+++ lib/Models/Post.dart
@@ -1,21 +1,22 @@
 import 'package:flare_parent/Models/Student.dart';
 
 class Post{
-  final String pID,url,date;
+  final String pID,url,date,dics;
   final Student uploader;  
   final List<Student> likes;
   final List<Comment> comments;
 
-  Post(this.pID, this.url, this.date, this.uploader, this.likes, this.comments);
+  Post(this.pID, this.url, this.date,this.dics, this.uploader, this.likes, this.comments);
 
 }
 
 class Comment{
+  final String comment;
   final Student user;
   final String date,time;
   final List<Reply> replies;
 
-  Comment(this.user, this.date, this.time, this.replies);
+  Comment(this.comment,this.user, this.date, this.time, this.replies);
 }
 
 class Reply{
--- lib/Screen/ActivityPage.dart
+++ lib/Screen/ActivityPage.dart
@@ -1,6 +1,8 @@
 import 'package:flare_parent/Models/Post.dart';
 import 'package:flare_parent/Models/Student.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
 
 class ActivityPage extends StatefulWidget {
   ActivityPage({Key key}) : super(key: key);
@@ -10,7 +12,8 @@ class ActivityPage extends StatefulWidget {
 }
 
 class _ActivityPageState extends State<ActivityPage> {
-  List<Post> posts;
+  List<Post> testposts;
+  Student _currentStudent;
 
   @override
   void initState() {
@@ -18,155 +21,279 @@ class _ActivityPageState extends State<ActivityPage> {
         "https://image.shutterstock.com/image-photo/double-exposure-image-business-man-260nw-769705507.jpg";
     Student testStudent = new Student(testProfile, "40", 'S1234', "John Doe",
         "11", 'c', 'farmer doe', 'miss doe', '12-6-2020', '1234567890', [], []);
-    posts = [
+    _currentStudent = new Student(testProfile, "40", 'S3921', "John Doe", "11",
+        'c', 'farmer doe', 'miss doe', '12-6-2020', '1234567890', [], []);
+   testposts = [
       new Post(
-          'p0001',
-          testProfile,
-          '01-07-2020',
-          testStudent,
-          [testStudent, testStudent],
-          [Comment(testStudent, '22-22-1784', '04:55:65', [])]),
-          new Post(
-          'p0001',
-          testProfile,
-          '01-07-2020',
-          testStudent,
-          [],
-          [Comment(testStudent, '22-22-1784', '04:55:65', [])]),
-          new Post(
-          'p0001',
-          testProfile,
-          '01-07-2020',
-          testStudent,
-          [testStudent, testStudent,testStudent,testStudent],
-          [Comment(testStudent, '22-22-1784', '04:55:65', [])]),
+          'p0001', testProfile, '01-07-2020', 'This is Message', testStudent, [
+        testStudent,
+        testStudent
+      ], [
+        Comment(
+            'This is Test Comment', testStudent, '22-22-1784', '04:55:65', [])
+      ]),
+      new Post('p0001', testProfile, '01-07-2020', 'This is Message',
+          testStudent, [], [
+        Comment(
+            'This is Test Comment', testStudent, '22-22-1784', '04:55:65', [])
+      ]),
+      new Post(
+          'p0001', testProfile, '01-07-2020', 'This is Message', testStudent, [
+        testStudent,
+        testStudent,
+        testStudent,
+        testStudent
+      ], [
+        Comment(
+            'This is Test Comment', testStudent, '22-22-1784', '04:55:65', [])
+      ]),
     ];
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: posts.length,
-      itemBuilder: (context, index) => Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return FutureBuilder<List<Post>>(
+        future: getPosts(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Container(
+                alignment: FractionalOffset.center,
+                child: CircularProgressIndicator());
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) => Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    new Container(
-                      height: 40.0,
-                      width: 40.0,
-                      decoration: new BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: new DecorationImage(
-                            fit: BoxFit.fill,
-                            image:
-                                new NetworkImage(posts[index].uploader.photoUrl)),
-                      ),
-                    ),
-                    new SizedBox(
-                      width: 10.0,
-                    ),
-                    new Text(
-                      posts[index].uploader.name,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-                new IconButton(
-                  icon: Icon(Icons.more_vert),
-                  onPressed: null,
-                )
+                _postCard( snapshot.data[index]),
               ],
             ),
-          ),
-          Flexible(
-            fit: FlexFit.loose,
-            child: new Image.network(
-              posts[index].url,
+          );
+        });
+  }
+
+  Future<List<Post>> getPosts() async{
+    List<Post> posts = [];
+    posts = testposts;
+    return posts;
+  }
+
+  Widget _postCard(Post post) {
+    bool isLiked = false;
+    for (int i = 0; i < post.likes.length; i++) {
+      if (post.likes[i].sID == _currentStudent.sID) {
+        isLiked = true;
+        break;
+      } else {
+        continue;
+      }
+    }
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 5),
+      child: InkWell(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  post.uploader.photoUrl.toString(),
+                ),
+              ),
+              contentPadding: EdgeInsets.all(0),
+              title: Text(
+                post.uploader.name,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              trailing: Text(
+                post.date,
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 11,
+                ),
+              ),
+            ),
+            Image.network(
+              post.url,
+              height: 170,
+              width: MediaQuery.of(context).size.width,
               fit: BoxFit.cover,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              color: Colors.grey,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  new Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      new IconButton(
-                          icon: Icon(
-                            Icons.thumb_up,
-                          ),
-                          onPressed: () {
-                            print("like");
-                          }),
-                      new SizedBox(
-                        width: 16.0,
-                      ),
-                      new IconButton(
-                          icon: Icon(
-                            Icons.comment,
-                          ),
-                          onPressed: () {
-                            print("comment");
-                          }),
-                      new SizedBox(
-                        width: 16.0,
-                      ),
-                    ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                    icon: Icon(isLiked
+                        ? FontAwesomeIcons.solidHeart
+                        : FontAwesomeIcons.heart),
+                    onPressed: () {
+                      setState(() {
+                        if (!isLiked) {
+                          post.likes.add(_currentStudent);
+                        } else {
+                          post.likes.remove(_currentStudent);
+                        }
+                      });
+                    }),
+                Text(
+                  '${post.likes.length} Likes',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 13,
                   ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              '${posts[index].likes.length.toString()} likes',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
-            child: Container(
-              color: Colors.grey,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  new SizedBox(
-                    width: 10.0,
+                ),
+                IconButton(
+                    icon: Icon(FontAwesomeIcons.comment), onPressed: () {}),
+                Text(
+                  '${post.comments.length} Comments',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 13,
                   ),
-                  Expanded(
-                    child: new TextField(
-                      decoration: new InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Add a comment...",
-                      ),
-                    ),
-                  ),
-                  IconButton(icon: Icon(Icons.send), onPressed: (){}),
-                ],
-              ),
+                ),
+              ],
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(posts[index].date, style: TextStyle(color: Colors.grey)),
-          ),
-          SizedBox(height: 20.0),
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  '${post.uploader.name}: ${post.dics}',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 16,
+                  ),
+                ),
+                MaterialButton(
+                    elevation: 0.0,
+                    color: Colors.transparent,
+                    child: Text('Add Comment',
+                        style: TextStyle(color: Colors.blue)),
+                    onPressed: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return new PostPage(
+                            post: post, currentStudent: _currentStudent);
+                      }));
+                    }),
+              ],
+            ),
+          ],
+        ),
+        onDoubleTap: () {
+          setState(() {
+            if (!isLiked) {
+              post.likes.add(_currentStudent);
+            } else {
+              post.likes.remove(_currentStudent);
+            }
+          });
+        },
+        onTap: () {},
       ),
     );
+  }
+}
+
+class PostPage extends StatefulWidget {
+  final Post post;
+  final Student currentStudent;
+  PostPage({Key key, @required this.post, @required this.currentStudent})
+      : super(key: key);
+
+  @override
+  _PostPageState createState() => _PostPageState(post, currentStudent);
+}
+
+class _PostPageState extends State<PostPage> {
+  final Post post;
+  final Student currentStudent;
+  final TextEditingController _commentController = TextEditingController();
+  _PostPageState(this.post, this.currentStudent);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Text('${post.uploader.name}\' Post'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: buildComments(),
+            ),
+            Divider(),
+            ListTile(
+              title: TextFormField(
+                controller: _commentController,
+                decoration: InputDecoration(labelText: 'Write a comment...'),
+              ),
+              trailing: OutlineButton(
+                onPressed: () {
+                  addComment(_commentController.text);
+                },
+                borderSide: BorderSide.none,
+                child: Text("Post"),
+              ),
+            ),
+          ],
+        ));
+  }
+
+  addComment(String msg) {
+    Comment comment = new Comment(
+        msg,
+        currentStudent,
+        new DateFormat("dd:MM:yyyy").format(new DateTime.now()),
+        new DateFormat("H:m:s").format(new DateTime.now()), []);
+    post.comments.insert(0, comment);
+    print(new DateFormat("dd:MM:yyyy").format(new DateTime.now()));
+    print(new DateFormat("H:m:s").format(new DateTime.now()));
+    setState(() {});
+  }
+
+  Widget buildComments() {
+    return FutureBuilder<List<Comment>>(
+        future: getComments(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Container(
+                alignment: FractionalOffset.center,
+                child: CircularProgressIndicator());
+
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  ListTile(
+                    title: Text(snapshot.data[index].comment),
+                    subtitle: Text(
+                      'By ${snapshot.data[index].user.name} on ${snapshot.data[index].date}',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(snapshot.data[index].user.photoUrl),
+                    ),
+                  ),
+                  Divider(),
+                ],
+              );
+            },
+          );
+        });
+  }
+
+  Future<List<Comment>> getComments() async {
+    List<Comment> comments = [];
+
+    comments = post.comments;
+
+    return comments;
   }
 }
